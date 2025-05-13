565d2d5b1e8dbeee36cffc67f37be332240712ff
catplnikhil <niraj@catpl.co.in>
2025-05-13 16:23:02 +0530

    .....

--- src/header/index.js
+++ src/header/index.js
@@ -51,7 +51,10 @@ const Headermain = () => {
                   <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
                   </li>
                   <li className="menu_item">
-                  <Link onClick={handleToggle} to="/about" className="my-3">About</Link>
+                  <Link onClick={handleToggle} to="#" className="my-3">Project</Link>
+                  </li>
+                  <li className="menu_item">
+                  <Link onClick={handleToggle} to="#" className="my-3">Certificate</Link>
                   </li>
                   <li className="menu_item">
                   <Link onClick={handleToggle} to="/contact" className="my-3"> Contact</Link>
